5737c7507606fe1016cf2a8134e626c2243815a2
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-12 00:51:59 -0300

    test 2

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG') == '1'
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = []
 if not DEBUG:
