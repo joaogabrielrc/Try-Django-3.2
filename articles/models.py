50ac981fc7f6c71bfd717587851ed21b35075265
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-13 00:18:37 -0300

    save 40

--- articles/models.py
+++ articles/models.py
@@ -1,40 +1,31 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
-from django.utils.text import slugify
+
+from .utils import slugify_instance_title
 
 
 class Article(models.Model):
   title = models.CharField(max_length=120)
-  slug = models.SlugField(blank=True, null=True)
+  slug = models.SlugField(unique=True, blank=True, null=True)
   content = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
 
-  def save(self, *args, **kwargs):    
+  def save(self, *args, **kwargs):   
+    # if self.slug is None:
+    #   slugify_instance_title(self, save=False) 
     super().save(*args, **kwargs)
 
 
-def slugify_instance_title(instance, save=False):
-  slug = slugify(instance.title)
-  queryset = Article.objects.filter(slug=slug).exclude(id=instance.id)
-  if queryset.exists():
-    slug = f'{slug}-{queryset.count() + 1}'
-  instance.slug = slug
-  if save:
-    instance.save()  
-
-
-def article_pre_save(sender, instance, *args, **kwargs):
-  print('pre_save')  
+def article_pre_save(sender, instance, *args, **kwargs):  
   if instance.slug is None:
     slugify_instance_title(instance, save=False)
 
 pre_save.connect(article_pre_save, sender=Article)
 
 
-def article_post_save(sender, instance, created, *args, **kwargs):
-  print('post_save')  
-  if created:
+def article_post_save(sender, instance, created, *args, **kwargs):  
+  if created:    
     slugify_instance_title(instance, save=True)
 
 post_save.connect(article_post_save, sender=Article)
